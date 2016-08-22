e9ba87775695db5c97e3b159f58668584030cdfb
koushals <koushal.majeti31@gmail.com>
2016-08-22 08:32:39 -0700

    renamed class

---
+++ IhearRadio/src/com/test/problem/Test_iheartRadio.java
@@ -0,0 +1,53 @@
+package com.test.problem;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.page.action.HomePage;
+import com.page.action.LoginPage;
+import com.page.action.MusicPlayerPage;
+import com.page.action.MySuggestionsPage;
+import com.page.driver.BaseClass;
+import com.test.utils.PropertyUtil;
+import com.test.utils.ShellUtils;
+
+import io.appium.java_client.AppiumDriver;
+
+
+public class Test_iheartRadio extends BaseClass{
+	
+	AppiumDriver<WebElement> driver;
+	private final static String fileName= "iHeartRadio.properties";
+	PropertyUtil p = new PropertyUtil(fileName);
+	
+	@BeforeTest
+	public void	createTheInstance() throws Exception{
+		 startDriverForApp(fileName);
+	}
+	
+	@Test
+	public void	rigisterAnewUser() throws Exception{
+		LoginPage.tapSkip();
+		String email = p.getPropertiesFromFile(fileName).getProperty("email");
+		String password = p.getPropertiesFromFile(fileName).getProperty("password");
+		LoginPage.loginPage(email, password);
+		HomePage.tapOnMySuggestion();
+		MySuggestionsPage.selectFromStations(0);
+		Assert.assertTrue(MusicPlayerPage.likeisDisplayed());
+		Assert.assertTrue(MusicPlayerPage.nextisDisplayed());
+		Thread.sleep(50000);
+	}
+	
+	@AfterTest
+	public void closeTheDevice() throws Exception{
+		takeScreenShot();
+		closeApp();
+		ShellUtils.uninstallApp(PropertyUtil.getPropertiesFromFile(fileName).getProperty("appPackage"));
+		acceptNotifications();
+	}
+
+
+}
